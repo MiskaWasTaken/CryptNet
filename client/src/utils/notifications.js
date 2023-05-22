e846387cdcacfe2d7c01669bd3e15955e9aeeb4e
miskawastaken <aaryamungli@gmail.com>
2023-05-23 00:05:34 +0530

    mobile support

--- client/src/utils/notifications.js
+++ client/src/utils/notifications.js
@@ -7,40 +7,43 @@ const showNotification = (title, message) => {
     silent: true, // we play our own sounds
   };
 
-  const notification = new Notification(title, notifBody);
-  const handleVisibilityChange = () => {
-    if (document.visibilityState === 'visible') {
-      // The tab has become visible so clear the now-stale Notification.
+  if ('Notification' in window) {
+    const notification = new Notification(title, notifBody);
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible') {
+        // The tab has become visible, so clear the now-stale Notification.
+        notification.close();
+        document.removeEventListener('visibilitychange', handleVisibilityChange);
+      }
+    };
+    // Focus window on click
+    notification.onclick = function () {
+      window.focus();
       notification.close();
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    }
-  };
-  // Focus window on click
-  notification.onclick = function () {
-    window.focus();
-    notification.close();
-  };
-  document.addEventListener('visibilitychange', handleVisibilityChange);
+    };
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+  } else {
+    alert('This browser does not support desktop notification');
+  }
 };
 
 export const notify = (title, content = '') => {
-  if (!('Notification' in window)) {
+  if ('Notification' in window) {
+    // Let's check whether notification permissions have already been granted
+    if (Notification.permission === 'granted') {
+      // If it's okay, let's create a notification
+      showNotification(title, content);
+    } else if (Notification.permission !== 'denied') {
+      // Otherwise, we need to ask the user for permission
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          showNotification(title, content);
+        }
+      });
+    }
+  } else {
     alert('This browser does not support desktop notification');
-  }
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === 'granted') {
-    // If it's okay let's create a notification
-    showNotification(title, content);
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === 'granted') {
-        showNotification(title, content);
-      }
-    });
   }
 };
 
